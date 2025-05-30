aa1d2c026e5f30ab3b020b54cf3ce0bed6c8cded
Iman Taheri <imantaheri1196@yahoo.com>
2025-05-30 10:58:41 +0330

    make the models and run the migrations

--- backend/models.py
+++ backend/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class CosmeticDoctor(models.Model):
+    name = models.CharField(max_length=100)
+    clinic = models.CharField(max_length=200)
+    specialty = models.CharField(max_length=100)
+    years_of_experience = models.PositiveIntegerField()
+    contact_number = models.IntegerField(max_length=20)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.full_name} - {self.specialty}"
